95efea4da7b4292fb13c11dee5c4e1442f12b992
5herw00d <dimamihajlov@gmail.com>
2026-07-19 18:57:56 +0200

    Remove extra borders on blog listing.
    Drop the hero bottom rule and the line above the first post row on /blog.

--- tests/localization.test.js
+++ tests/localization.test.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { relatedPostsFor } = require('../scripts/blog-data.js');
 
 const ROOT = path.resolve(__dirname, '..');
-const STYLE_VERSION = '20260718-9';
+const STYLE_VERSION = '20260718-10';
 
 function read(rel) {
   return fs.readFileSync(path.join(ROOT, rel), 'utf8');
